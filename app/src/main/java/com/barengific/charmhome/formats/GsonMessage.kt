6931047c086f2a522b9e2cdebad39cf32908f6f5
Barengific <75044077+Barengific@users.noreply.github.com>
2022-02-02 20:31:45 +0000

    Update GsonMessage.kt

--- app/src/main/java/com/barengific/charmhome/formats/GsonMessage.kt
+++ app/src/main/java/com/barengific/charmhome/formats/GsonMessage.kt
@@ -2,7 +2,6 @@ package com.barengific.charmhome.formats
 
 import org.http4k.core.Body
 import org.http4k.format.Gson.auto
-import java.lang.String.format
 
 data class GsonMessage(val subject: String, val message: String)
 
