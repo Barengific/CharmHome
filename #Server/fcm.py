831e4f3f1623697f9f4950937fde8380ef86f51e
Barengific <75044077+Barengific@users.noreply.github.com>
2022-02-02 09:23:55 +0000

    Update fcm.py

--- #Server/fcm.py
+++ #Server/fcm.py
@@ -5,5 +5,5 @@ from firebase_admin import credentials
 
 default_app = firebase_admin.initialize_app()
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
