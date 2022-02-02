d8aaccaaf494197fe1ab3ede16bc613da2b26128
Barengific <75044077+Barengific@users.noreply.github.com>
2022-02-02 11:25:13 +0000

    Update MainActivity.kt

--- app/src/main/java/com/barengific/charmhome/MainActivity.kt
+++ app/src/main/java/com/barengific/charmhome/MainActivity.kt
@@ -2,10 +2,29 @@ package com.barengific.charmhome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            val TAG = "aaaaaToken"
+            if (!task.isSuccessful) {
+                Log.d(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
+
+        FirebaseMessaging.getInstance().getToken()
     }
 }
