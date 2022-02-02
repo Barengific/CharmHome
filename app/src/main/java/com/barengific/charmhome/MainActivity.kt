2693b42b388ea59a61bf398287426799ad8a5331
Barengific <75044077+Barengific@users.noreply.github.com>
2022-02-02 13:20:30 +0000

    Update MainActivity.kt

--- app/src/main/java/com/barengific/charmhome/MainActivity.kt
+++ app/src/main/java/com/barengific/charmhome/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.barengific.charmhome
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.media.RingtoneManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import com.google.android.gms.tasks.OnCompleteListener
@@ -18,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Firebase.messaging.isAutoInitEnabled = true
+        FirebaseMessaging.getInstance()
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             val TAG = "aaaaaToken"
@@ -31,10 +39,48 @@ class MainActivity : AppCompatActivity() {
 
             // Log and toast
             Log.d(TAG, token)
-            Toast.makeText(baseContext, token, Toast.LENGTH_LONG).show()
+//            Toast.makeText(baseContext, token, Toast.LENGTH_LONG).show()
         })
 
         val tokenss =  FirebaseMessaging.getInstance().getToken()
+        createNotificationChannel()
+        sendNoti()
+    }
+
+
+    private fun sendNoti(){
+        val CHANNEL_ID = "mychannel_1"
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("yup")
+            .setContentText("ahaha")
+            .setSound(defaultSoundUri)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(12345, builder.build())
+            Log.d("aaaMainNoti", "notified")
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name ="notiChannelName"
+            val descriptionText = "this is basic channel desc"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val CHANNEL_ID = "mychannel_1"
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }//TODO
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
