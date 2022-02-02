8c190763662c8c40bf801bec6b1f45f9c8969536
Barengific <75044077+Barengific@users.noreply.github.com>
2022-02-02 20:18:39 +0000

    Create GsonMessage.kt

---
+++ app/src/main/java/com/barengific/charmhome/formats/GsonMessage.kt
@@ -0,0 +1,8 @@
+package com.barengific.charmhome.formats
+
+import org.http4k.core.Body
+import org.http4k.format.Gson.auto
+
+data class GsonMessage(val subject: String, val message: String)
+
+val gsonMessageLens = Body.auto<GsonMessage>().toLens()
